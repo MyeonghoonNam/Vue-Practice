ba3110fbac34ba7114ec7866912e2886bf103e26
MyeongHoonNam <codenmh0822@naver.com>
2021-10-06 04:37:04 +0900

    eslint 설정 하기

--- SFC 프로젝트 구성/Webpack/src/App.vue
+++ SFC 프로젝트 구성/Webpack/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <h1
+      v-if="msg"
+      class="message"
+      @click="log"
+    >
+      {{ msg }}
+    </h1>
+    <div></div>
     <Hello />
   </div>
 </template>
 
 <script>
-import Hello from '~/components/Hello';
+import Hello from '~/components/Hello'
 
 export default {
   components: {
@@ -15,7 +22,7 @@ export default {
   data() {
     return {
       msg: 'Hello Vue ~!',
-    };
+    }
   },
-};
+}
 </script>
