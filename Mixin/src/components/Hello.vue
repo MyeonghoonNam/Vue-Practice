69f6e1cf6b69403106e43a6b88f29c5208b1c13f
MyeongHoonNam <codenmh0822@naver.com>
2021-10-07 01:16:02 +0900

    mixin 기초 사용법

---
+++ Mixin/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+import sampleMixin from '~/mixins/sample'
+
+export default {
+  mixins: [sampleMixin]  
+}
+</script>
+
+<style scoped lang="scss">
+$color: orange;
+
+h1 {
+  color: $color;
+}
+</style>
