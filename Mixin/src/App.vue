75ccfb2e1747cab0759632b6cdf76468c59d3ce9
MyeongHoonNam <codenmh0822@naver.com>
2021-10-07 02:21:59 +0900

    설문조사 애플리케이션 제작을 통한 믹스인 활용하기

--- Mixin/src/App.vue
+++ Mixin/src/App.vue
@@ -1,22 +1,69 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <h1>{{ count }}</h1>
-  <Hello />
+  <h1>설문조사</h1>
+  <!-- <TextField
+    v-model="fields[0].value"
+    :title="fields[0].title"
+  />
+  <SimpleRadio
+    v-model="fields[1].value"
+    :title="fields[1].title"
+    :items="fields[1].items"
+  /> -->
+  <component
+    :is="field.component"
+    v-for="field in fields"
+    :key="'component-' + field.title"
+    v-model="field.value"
+    :title="field.title"
+    :items="field.items"
+  />
+
+  <h1>결과</h1>
+  <div
+    v-for="field in fields"
+    :key="'rersult-' + field.title"
+  >
+    {{ field.value }}
+  </div>
+
+  <button @click="submit">
+    제출 !
+  </button>
 </template>
 
 <script>
-import Hello from '~/components/Hello'
-import sampleMixin from '~/mixins/sample'
+import * as FieldComponents from '~/components/fields/index.js'
 
 export default {
   components: {
-    Hello,
+    ...FieldComponents
   },
-  mixins: [sampleMixin],
   data() {
     return {
-      msg: 'Hello Vue ~!',
+      fields: [
+        {
+          component: 'TextField',
+          title: '이름',
+          value: ''
+        },
+        {
+          component: 'SimpleRadio',
+          title: '나이대',
+          value: '',
+          items: ['20대', '30대', '40대', '50대',]
+        },
+      ]
     }
   },
+  methods: {
+    submit() {
+      const results = this.fields.map(({title, value}) => ({
+        title,
+        value
+      }))
+
+      console.log(results)
+    }
+  }
 }
 </script>
---
+++ Mixin/src/components/fields/TextField.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3> {{ title }}</h3>
+  <!-- eslint-disable -->
+  <input
+    type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  /> 
+  <!-- eslint-enable -->
+</template>
+
+<script>
+import fieldMixin from './mixin'
+
+export default {
+  mixins: [fieldMixin]
+}
+</script>
