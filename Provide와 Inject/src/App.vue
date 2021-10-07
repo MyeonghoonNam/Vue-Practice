fc58ec4cd814b16eb3f5cee2a49241689cade21c
MyeongHoonNam <codenmh0822@naver.com>
2021-10-07 17:02:50 +0900

    SFC 웹팩 초기 설정

---
+++ Provide와 Inject/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1
+      v-if="msg"
+      class="message"
+      @click="log"
+    >
+      {{ msg }}
+    </h1>
+    <div></div>
+    <Hello />
+  </div>
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: 'Hello Vue ~!',
+    }
+  },
+}
+</script>
