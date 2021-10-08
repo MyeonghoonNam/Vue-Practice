83738a53792c7a2428e25a9ce559aec65df7ff34
MyeongHoonNam <codenmh0822@naver.com>
2021-10-08 14:40:48 +0900

    라우터 기초

---
+++ 라우터/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <RouterLink to="/">
+    Home
+  </RouterLink>
+  <RouterLink to="/about">
+    About
+  </RouterLink> -->
+  <button @click="$router.push({ name: 'home' })">
+    Home
+  </button>
+  <button @click="$router.push({ name: 'about' })">
+    About
+  </button>
+  <button
+    @click="$router.push({ 
+      name: 'docsId',
+      params: { id: '7777' },
+      query: { name: 'Hoon', age: 26, email: 'hoon@abc.com'}
+    })"
+  >
+    Documents Id
+  </button>
+  <RouterView />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello Vue ~!',
+    }
+  },
+}
+</script>
